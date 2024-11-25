ba6a648aa0214f02e98fd0dc1ce5371edbc1e179
chandra kurniawan <chandra.kurniawan@formulatrix.com>
2024-11-25 22:44:40 +0700

    v2-probe

--- config/Configuration.h
+++ config/Configuration.h
@@ -1418,7 +1418,7 @@
  *    - Normally-closed (NC) also connect to GND.
  *    - Normally-open (NO) also connect to 5V.
  */
-//#define Z_MIN_PROBE_PIN -1
+#define Z_MIN_PROBE_PIN -1
 
 /**
  * Probe Type
