b52ad83b74af0e723650f4222c7f4a7f4969a020
chandra kurniawan <chandra.kurniawan@formulatrix.com>
2024-12-02 21:41:29 +0700

    v5__0

--- config/Configuration.h
+++ config/Configuration.h
@@ -1637,8 +1637,8 @@
  *     O-- FRONT --+
  */
 
-#define X_PROBE_OFFSET 25
-#define Y_PROBE_OFFSET -31
+#define X_PROBE_OFFSET 10
+#define Y_PROBE_OFFSET 10
 #define Z_PROBE_OFFSET -2.80
 
 #define NOZZLE_TO_PROBE_OFFSET { X_PROBE_OFFSET, Y_PROBE_OFFSET, Z_PROBE_OFFSET }
@@ -1885,11 +1885,11 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 180
-#define Y_BED_SIZE 180
+#define X_BED_SIZE 145
+#define Y_BED_SIZE 145
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS -10
+#define X_MIN_POS -5
 #define Y_MIN_POS -7
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
@@ -2346,8 +2346,8 @@
 #define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
-  #define Z_SAFE_HOMING_X_POINT ((X_BED_SIZE) / 2) // (mm) X point for Z homing
-  #define Z_SAFE_HOMING_Y_POINT ((Y_BED_SIZE) / 2) // (mm) Y point for Z homing
+  #define Z_SAFE_HOMING_X_POINT X_CENTER // (mm) X point for Z homing
+  #define Z_SAFE_HOMING_Y_POINT Y_CENTER // (mm) Y point for Z homing
   //#define Z_SAFE_HOMING_POINT_ABSOLUTE  // Ignore home offsets (M206) for Z homing position
 #endif
 
